fc45b74f0416f5552d56490afbe000055a129aaa
viste <viste02@gmail.com>
2023-02-18 18:29:28 +0300

    revert

--- core/__init__.py
+++ core/__init__.py
@@ -5,11 +5,8 @@ from misc.utils import config
 def setup_routers() -> Router:
     from . import callbacks, admins, admin_handler, user_handler, misc
 
-    user = 0
     router = Router()
     router.message.filter(F.chat.id != config.banned_group)
-    if user in config.banned_users:
-        router.message.filter(F.user.id != user)
 
     router.include_router(misc.router)
     router.include_router(admin_handler.router)
