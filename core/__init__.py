f060dfad27b11d94692176b96e7c2a7be119bfd2
viste <viste02@gmail.com>
2023-02-18 18:28:47 +0300

    fuck scam

--- core/__init__.py
+++ core/__init__.py
@@ -5,9 +5,11 @@ from misc.utils import config
 def setup_routers() -> Router:
     from . import callbacks, admins, admin_handler, user_handler, misc
 
+    user = 0
     router = Router()
     router.message.filter(F.chat.id != config.banned_group)
-    router.message.filter(F.user.id not in config.banned_users)
+    if user in config.banned_users:
+        router.message.filter(F.user.id != user)
 
     router.include_router(misc.router)
     router.include_router(admin_handler.router)
