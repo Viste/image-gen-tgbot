e204f86c7763f0f1f4f0d5b32d85ea5af70c0fd5
viste <viste02@gmail.com>
2023-03-06 06:22:57 +0300

    update

--- misc/bridge.py
+++ misc/bridge.py
@@ -51,8 +51,8 @@ class OpenAI:
 
         # Generate response
         gpt = OpenAI()
-        task = gpt.send_to_davinci.app.task.apply_async(args=[conversation_history])
-        response = task.app.task.get()
+        task = gpt.send_to_davinci.apply_async(args=[conversation_history])
+        response = task.get()
 
         user_responses.append(response)
         conversations[user_id] = {'conversations': user_messages, 'responses': user_responses}
