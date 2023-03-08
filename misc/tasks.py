0d7339707b25efef67abc66db2a9e27021d7470d
viste <viste02@gmail.com>
2023-03-08 22:50:00 +0300

    update

--- misc/tasks.py
+++ misc/tasks.py
@@ -7,7 +7,7 @@ openai.api_key = config.api_key
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=5, retry_jitter=True, retry_kwargs={'max_retries': 5})
-def send_davinci(message_text):
+def send_davinci(message_text: str):
     model = "text-davinci-003"
     text = "You are an AI named Настя and you are in a conversation with a human. You can answer questions," \
            "provide information, and help with a wide variety of tasks. You are good at writing clean and standard " \
