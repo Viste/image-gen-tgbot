4f17f3db43cf65117750b93c4fb3ddcf4b6c0caf
viste <viste02@gmail.com>
2023-04-15 02:39:45 +0300

    update
    Signed-off-by: viste <viste02@gmail.com>

--- core/commands.py
+++ core/commands.py
@@ -68,7 +68,7 @@ async def process_ask(message: types.Message) -> None:
                     await message.reply(error, parse_mode=None)
 
 
-@router.message(F.reply_to_message.from_user.is_bot)
+@router.message(F.from_user.is_bot)
 async def process_ask(message: types.Message) -> None:
     uid = message.from_user.id
     if uid in config.banned_user_ids:
