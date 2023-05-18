f20b9658c6c0bd55453d933ebeb0064dcd3a4028
viste <viste02@gmail.com>
2023-05-18 05:29:14 +0300

    upd
    Signed-off-by: viste <viste02@gmail.com>

--- artint/mj/receiver.py
+++ artint/mj/receiver.py
@@ -68,9 +68,8 @@ class Receiver:
                         prompt = message['content'].split('**')[1].split(' --')[0]
                         url = message['attachments'][0]['url']
                         filename = message['attachments'][0]['filename']
-                        timestamp = message['timestamp']
-                        if id not in self.df.index:
-                            self.df.loc[message_id] = [prompt, url, filename, timestamp, 0]
+                        if message_id not in self.df.index:
+                            self.df.loc[message_id] = [prompt, url, filename, message_timestamp, 0]
                             self.latest_image_timestamp = parse(message["timestamp"])
 
                     else:
