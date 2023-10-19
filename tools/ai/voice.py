8973fd9df0631a01bafcfe2ad9706f5845c98c9f
viste <viste02@gmail.com>
2023-10-19 13:51:18 +0300

    update voice settings
    Signed-off-by: viste <viste02@gmail.com>

--- tools/ai/voice.py
+++ tools/ai/voice.py
@@ -10,7 +10,7 @@ class ELAI:
     def __init__(self):
         super().__init__()
         self.key = config.el_key
-        self.url = "https://api.elevenlabs.io/v1/text-to-speech/dWvQRPctX7BT3AMBjjdX/stream"
+        self.url = "https://api.elevenlabs.io/v1/text-to-speech/dWvQRPctX7BT3AMBjjdX"
         self.model = "eleven_monolingual_v2"
         self.headers = {
             "Accept": "audio/mpeg",
@@ -24,7 +24,7 @@ class ELAI:
     async def _send_req(self, url, data):
         async with aiohttp.ClientSession() as session:
             async with session.post(url, headers=self.headers, json=data) as resp:
-                logger.info("SEND REQUEST LOGGER %s",resp)
+                logger.info("SEND REQUEST LOGGER %s", resp)
                 return await resp.read()
 
     async def send2api(self, text, uid):
@@ -33,7 +33,9 @@ class ELAI:
             "text": text,
             "voice_settings": {
                 "stability": 1,
-                "similarity_boost": 0.3
+                "similarity_boost": 0.9,
+                "style": 0.6,
+                "use_speaker_boost": True
                 }
             })
         content = await self._send_req(self.url, data)
