fa0fbac408c241616781d8d0208bed46a16ccd0a
viste <viste02@gmail.com>
2023-11-01 23:29:02 +0300

    maybe fix voice maybe not
    Signed-off-by: viste <viste02@gmail.com>

--- tools/ai/voice.py
+++ tools/ai/voice.py
@@ -10,7 +10,7 @@ class ELAI:
     def __init__(self):
         super().__init__()
         self.key = config.el_key
-        self.url = "https://api.elevenlabs.io/v1/text-to-speech/dWvQRPctX7BT3AMBjjdX"
+        self.url = "https://api.elevenlabs.io/v1/text-to-speech/dWvQRPctX7BT3AMBjjdX?optimize_streaming_latency=0"
         self.model = "eleven_monolingual_v2"
         self.headers = {
             "Accept": "audio/mpeg",
