ef4c7dc2c17764a5283458dbdc2ed52a1aa22d1f
viste <viste02@gmail.com>
2023-11-02 01:41:55 +0300

    fix save
    Signed-off-by: viste <viste02@gmail.com>

--- tools/ai/voice_new.py
+++ tools/ai/voice_new.py
@@ -16,6 +16,4 @@ class ELAI:
     async def send2api(self, text, uid):
         content = self.voice.generate_audio_v2(text, GenerationOptions(model="eleven_multilingual_v2", stability=0.94, style=0.5, similarity_boost=0.75))
         save_audio_bytes(content[0], f'{str(uid)}.mp3', outputFormat="mp3")
-        with open(f'{str(uid)}.mp3', 'wb') as f:
-            f.write(content)
         return f'{str(uid)}.mp3'
